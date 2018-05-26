325d61b6c7858decb48e2cda2bd0ceb94dcbf305
Andrew Rabert <ar@nullsum.net>
2018-05-26 01:13:14 -0400

    Start Docker on boot

--- bin/provision_coreos.sh
+++ bin/provision_coreos.sh
@@ -3,3 +3,4 @@ set -ex
 
 ssh-copy-id -i ~/.ssh/nullsum.net core@nullsum.net
 ssh -i ~/.ssh/nullsum.net core@nullsum.net '~/bin/install_docker_compose_coreos.sh'
+ssh -i ~/.ssh/nullsum.net core@nullsum.net sudo systemctl enable docker
