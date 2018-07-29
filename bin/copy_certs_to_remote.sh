5ff3cd56389ce3bd8a837d9e5729a0b488be4fad
Andrew Rabert <ar@nullsum.net>
2018-07-29 18:00:19 -0400

    git-auto-update 2018-07-29 18:00:19

---
+++ bin/copy_certs_to_remote.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+set -ex
+
+rsync -avz --progress --delete -e ssh ~/Code/GitLab/nvllsvm/certs-nullsum.net/certs/ nullsum.net:'~/certs'
--- bin/copy_to_remote.sh
+++ bin/copy_to_remote.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 set -ex
 
+ssh nullsum.net mkdir -p "~/certs"
 rsync -avz --progress --delete -e ssh bin/ nullsum.net:'~/bin'
 rsync -avz --progress --delete -e ssh docker/ nullsum.net:'~/docker'
 rsync -avz --progress --delete -e ssh output/ nullsum.net:'~/content'
