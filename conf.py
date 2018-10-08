58822b3284a4b75e1c8ce7a4f0775952963fb453
Andrew Rabert <ar@nullsum.net>
2018-10-07 23:56:25 -0400

    Update versions

--- conf.py
+++ conf.py
@@ -216,7 +216,7 @@ TIMEZONE = "America/New_York"
 
 # Date format used to display post dates. (translatable)
 # (str used by datetime.datetime.strftime)
-DATE_FORMAT = '%Y-%m-%d'
+DATE_FORMAT = 'YYYY-MM-dd'
 
 # Date format used to display post dates, if local dates are used. (translatable)
 # (str used by moment.js)
