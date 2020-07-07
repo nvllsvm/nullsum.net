416531d4ca44a03860dfe91c6cbe51ef08036ad1
Andrew Rabert <ar@nullsum.net>
2020-07-07 10:03:35 -0400

    Set COMPOSE_FILE in ~/.profile

--- bin/provision_alpine.sh
+++ bin/provision_alpine.sh
@@ -14,3 +14,7 @@ service sshd restart
 apk add docker docker-compose rsync
 rc-update add docker boot
 service docker start
+
+cat > ~/.profile << EOF
+export COMPOSE_FILE=~/docker/docker-compose.yml
+EOF
