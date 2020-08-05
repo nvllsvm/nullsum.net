a9b2f0893ac4785919b5a3f2abf4709e6393087d
Andrew Rabert <ar@nullsum.net>
2020-08-05 09:39:11 -0400

    update folder paths

--- bin/copy_to_remote.sh
+++ bin/copy_to_remote.sh
@@ -4,4 +4,4 @@ set -ex
 ssh nullsum.net mkdir -p "~/certs"
 rsync -avz --progress --delete -e ssh bin/ nullsum.net:'~/bin'
 rsync -avz --progress --delete -e ssh docker/ nullsum.net:'~/docker'
-rsync -avz --progress --delete -e ssh public/ nullsum.net:'~/blog'
+rsync -avz --progress --delete -e ssh public/ nullsum.net:'~/srv/http/www'
