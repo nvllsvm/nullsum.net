e281578b34c696c73a5d476e7230d0ed285f4349
Andrew Rabert <ar@nullsum.net>
2024-09-25 06:44:20 -0400

    Fix

--- bin/copy_certs_to_remote.sh
+++ bin/copy_certs_to_remote.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 set -e
 
-rsync -avz --progress --delete -e ssh ~/src/github/nvllsvm/certs-nullsum.net/certs/ nullsum.net:'~/certs'
+rsync -avz --progress --delete -e ssh ~/src/git.nullsum.net/nvllsvm/certs-nullsum.net/certs/ nullsum.net:'~/certs'
